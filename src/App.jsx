f9aea0698b9951eabab95c3eba1559f7e5033953
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-07-19 15:41:53 +0100

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [preState, setPreState] = useState("");
+  const [curState, setCurState] = useState("");
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
+
+  const inputNum = e => {}
+
+  const operatorType = e => {}
+
+  const equals = e => {}
+
+  const minusPlus = () => {}
+
+  const percent = () => {}
+
+  const reset = () => {}
 
   return (
-    <div></div>
-  )
+    <div className="container">
+      <div className="wrapper">
+        <div className="screen"></div>
+        <div className='btn light-gray' onClick={reset}>AC</div>
+        <div className='btn light-gray' onClick={percent}>%</div>
+        <div className='btn light-gray' onClick={minusPlus}>+/-</div>
+        <div className='btn orange' onClick={operatorType}>/</div>
+        <div className='btn' onClick={inputNum}>7</div>
+        <div className='btn' onClick={inputNum}>8</div>
+        <div className='btn' onClick={inputNum}>9</div>
+        <div className='btn orange' onClick={operatorType}>X</div>
+        <div className='btn' onClick={inputNum}>4</div>
+        <div className='btn' onClick={inputNum}>5</div>
+        <div className='btn' onClick={inputNum}>6</div>
+        <div className='btn orange' onClick={operatorType}>+</div>
+        <div className='btn' onClick={inputNum}>1</div>
+        <div className='btn' onClick={inputNum}>2</div>
+        <div className='btn' onClick={inputNum}>3</div>
+        <div className='btn orange' onClick={operatorType}>-</div>
+        <div className='btn zero' onClick={inputNum}>0</div>
+        <div className='btn' onClick={inputNum}>.</div>
+        <div className='btn' onClick={equals}> = </div>
+      </div>
+    </div>
+  );
 }
 
 export default App
